572f538c4e165aa6386c283eecb867e450611b87
md ismail <mdismail85773@gmail.com>
2025-09-19 03:04:54 +0800

    favorite items function added

--- src/components/favorite/Favorite.jsx
+++ src/components/favorite/Favorite.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { RiDeleteBin2Line } from "react-icons/ri";
 
-const Favorite = ({favorite}) => {
+const Favorite = ({favorite, removeBtn}) => {
     // console.log(favorite)
     return (
-        <div>
-            <h1 className='mb-14' >Favorite Items: {0} </h1>
-            <img className='h-[300px] w-[500px]' src={favorite.image} alt="" />
+        <div className='mt-14'>
+            <div className="relative w-[500px] h-[300px]">
+                <img
+                className="h-full w-full object-cover "
+                src={favorite.image}
+                alt=""
+                />
+
+                <button 
+                    onClick={() => removeBtn(favorite.id)} 
+                    className="absolute top-2 right-2 text-white bg-black/50 p-2 rounded-full hover:bg-black/70 mr-5">
+                    <RiDeleteBin2Line size={24} />
+                </button>
+            </div>
+
             <span>{favorite.title}</span>
-            
         </div>
     );
 };
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,24 +4,32 @@ import { GiSelfLove } from "react-icons/gi";
 import Product from '../product/Product';
 import Favorite from '../favorite/Favorite';
 const Products = () => {
-   const [products, setProducts]= useState([]); // this is for api
-   const [favorite, setFavorite]= useState([]); // this is for bid btn to show favorite items
+    const [products, setProducts]= useState([]); // this is for api
+    const [favorite, setFavorite]= useState([]); // this is for bid btn to show favorite items
 
 
-useEffect(()=>{                         // data fetch
+// data fetch function
+useEffect(()=>{                         
     fetch("/public/products.json")
     .then(res => res.json())
     .then(data => setProducts(data))
 },[])
 // console.log(products)
 
-
-const handleBidBtn =(product)=>{         // bid button function
+// bid button function
+const handleBidBtn =(product)=>{         
 // console.log( product)
     setFavorite([...favorite , product])
     // console.log(favorite)
 }
 
+// favorite remove function
+const removeBtn =(id)=>{
+    // console.log("remove button added", id)
+const remainingFavorite = favorite.filter(favorite =>favorite.id !==id )
+    setFavorite(remainingFavorite)
+
+}                                   
 
     return (
         <div className='w-11/12 mx-auto mt-10  '> 
@@ -33,10 +41,12 @@ const handleBidBtn =(product)=>{         // bid button function
 
                  {/* favorite items section */}
                 <div className='text-3xl w-[30%] text-center  '>
+                    <h1>Favorite Items:</h1>
                         {
                             favorite.map(favorite => <Favorite favorite={favorite}
-                                                         key={favorite.id} >
-                                                    </Favorite>)                            
+                                                         key={favorite.id}
+                                                         removeBtn = {removeBtn} 
+                                                         ></Favorite>)                            
                         }
                  </div>   
             </div>
